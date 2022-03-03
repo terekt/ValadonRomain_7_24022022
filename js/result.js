2f4d248f916b16d81c501a3028b63764d18e846e
Romainv78 <romainvaladon@gmail.com>
2022-03-03 14:53:06 +0100

    ajout de commentaire et optimisation du code

--- js/result.js
+++ js/result.js
@@ -1,56 +1,37 @@
-/*function BinarySearchJSON (List,Index,ColumnName) {
-    var First = 0
-    var Last = List.length - 1
-    while (First <= Last) {
-       var Middle = Math.floor((First+Last)/2)
-       if (List[Middle][ColumnName] < Index) {
-         First = Middle + 1
-       } else if (List[Middle][ColumnName] > Index) {
-         Last = Middle - 1
-       } else {
-         return List[Middle]
-       }
-    }
-    return false
-}
-
-console.log(BinarySearchJSON(recipes,1,"id"))
-console.log(BinarySearchJSON(recipes,9,"id"))
-console.log(BinarySearchJSON(recipes,12,"id"))*/
-
 const searchinput = document.getElementById("searchinput")
 
-searchinput.addEventListener("keyup", function () {
-    const input = searchinput.value;
-    if (input.length > 2) {
-        var result = recipes.filter(function (item) {
-            const name = item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // récupère les recettes dont le nom correspond
+searchinput.addEventListener("keyup", function () { // quand on tape une touche du clavier
+    const input = searchinput.value; // récupère la valeur de la barre de recherche
+
+    if (input.length > 2) { // si on a plus de 2 charactères
+        var result = recipes.filter(function (item) { // crée et retourne un nouveau tableau à partir de la base de donnée
+            const name = item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // récupère les recettes dont le nom correspond à la recherche
             const description = item.description.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // description
 
             for (let i = 0; i < item.ingredients.length; i++) { // ingrédients
                 var ingredientList = item.ingredients[i].ingredient.toLocaleLowerCase().includes(input.toLocaleLowerCase());
             }
 
-            return (name || description || ingredientList)
+            return (name || description || ingredientList) // retourne les éléments correspondants aux 3 critères
         })
-    } else {
-        result = "En attente de recherche"
+    }
+    else { //sinon revois une variable vide
+        result = "";
     }
 
-    if (result == "") {
-        result = "Aucun résultat"
-    }
     console.log(result);
 
     //met à jour l'affichage des recette via le résultat de recherche
     var recetteSection = document.querySelector(".resultat-container");
 
     recetteSection.innerHTML = "";
-    result.forEach((recette) => {
-        const recetteModel = recetteFactory(recette);
-        const recetteDOM = recetteModel.recetteDOM();
-        recetteSection.appendChild(recetteDOM);
-    });
+    if (result != "") { // si la recherche n'est pas vide
+        result.forEach((recette) => { // créé et insère un élément html pour chaque élément de la liste
+            const recetteModel = recetteFactory(recette);
+            const recetteDOM = recetteModel.recetteDOM();
+            recetteSection.appendChild(recetteDOM);
+        });
+    }
 })
 
 
@@ -58,13 +39,6 @@ searchinput.addEventListener("keyup", function () {
 
 function recetteFactory(data) {
     const { id, name, ingredients, time, description } = data;
-
-    //console.log(ingredients);
-
-    /*for (let i = 0; i < ingredients.length; i++) { // ingrédients
-        var ingredientItem = ingredients[i].ingredient;
-        console.log(ingredientItem);
-    }*/
 
     function recetteDOM() {
         const recetteList = document.createElement("div");
@@ -90,13 +64,13 @@ function recetteFactory(data) {
             var ingredientQuantity = ingredients[i].quantity;
             var ingredientMetric = ingredients[i].unit;
 
-            if (ingredientMetric == "grammes") {
+            if (ingredientMetric == "grammes") { // réduit la taille l'unité de mesure pour correspondre aux maquettes
                 ingredientMetric = "g";
             }
             if (ingredientMetric == "cuillères à soupe") {
                 ingredientMetric = "cuillères";
             }
-            if (ingredientMetric == undefined) {
+            if (ingredientMetric == undefined) { // si aucune unité de mesure n'est récupéré, renvoyer une valeure vide
                 ingredientMetric = "";
             }
 
