145b59d70aa9caa848c5829846fad4095a961176
Romainv78 <romainvaladon@gmail.com>
2022-03-03 12:46:23 +0100

    visuel recettes

--- js/result.js
+++ js/result.js
@@ -20,10 +20,19 @@ console.log(BinarySearchJSON(recipes,12,"id"))*/
 
 const searchinput = document.getElementById("searchinput")
 
-searchinput.addEventListener("keyup", function(){
+searchinput.addEventListener("keyup", function () {
     const input = searchinput.value;
     if (input.length > 2) {
-        var result = recipes.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
+        var result = recipes.filter(function (item) {
+            const name = item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // récupère les recettes dont le nom correspond
+            const description = item.description.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // description
+
+            for (let i = 0; i < item.ingredients.length; i++) { // ingrédients
+                var ingredientList = item.ingredients[i].ingredient.toLocaleLowerCase().includes(input.toLocaleLowerCase());
+            }
+
+            return (name || description || ingredientList)
+        })
     } else {
         result = "En attente de recherche"
     }
@@ -32,4 +41,85 @@ searchinput.addEventListener("keyup", function(){
         result = "Aucun résultat"
     }
     console.log(result);
+
+    //met à jour l'affichage des recette via le résultat de recherche
+    var recetteSection = document.querySelector(".resultat-container");
+
+    recetteSection.innerHTML = "";
+    result.forEach((recette) => {
+        const recetteModel = recetteFactory(recette);
+        const recetteDOM = recetteModel.recetteDOM();
+        recetteSection.appendChild(recetteDOM);
+    });
 })
+
+
+
+
+function recetteFactory(data) {
+    const { id, name, ingredients, time, description } = data;
+
+    //console.log(ingredients);
+
+    /*for (let i = 0; i < ingredients.length; i++) { // ingrédients
+        var ingredientItem = ingredients[i].ingredient;
+        console.log(ingredientItem);
+    }*/
+
+    function recetteDOM() {
+        const recetteList = document.createElement("div");
+        recetteList.setAttribute("class", "resultat");
+
+        let card = "";
+
+        card += `
+        <img src="img/coco.jpeg" alt="${name}">
+        <div class="resultat-info">
+            <div class="titre-container">
+                <div class="nom">${name}</div>
+                <div class="temps">
+                    <i class="fas fa-clock"></i>
+                    <p>${time} min</p>
+                </div>
+            </div>
+            <div class="description-container">
+                <div class="ingredients">`
+
+        for (let i = 0; i < ingredients.length; i++) { // ingrédients
+            var ingredientItem = ingredients[i].ingredient;
+            var ingredientQuantity = ingredients[i].quantity;
+            var ingredientMetric = ingredients[i].unit;
+
+            if (ingredientMetric == "grammes") {
+                ingredientMetric = "g";
+            }
+            if (ingredientMetric == "cuillères à soupe") {
+                ingredientMetric = "cuillères";
+            }
+            if (ingredientMetric == undefined) {
+                ingredientMetric = "";
+            }
+
+            card += `<div><b>${ingredientItem}:</b> ${ingredientQuantity} ${ingredientMetric}</div>`
+        }
+
+        card += `</div>
+                <div class="texte-recette">
+                    <p>${description}</p>
+                </div>
+            </div>
+        </div>`;
+
+        recetteList.innerHTML = card;
+
+        return recetteList;
+    }
+    return {
+        id,
+        name,
+        ingredients,
+        time,
+        description,
+        recetteDOM,
+    };
+}
