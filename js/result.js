633b7933c32a980c99a689e70191e31e41a4caf3
Romainv78 <romainvaladon@gmail.com>
2022-03-10 16:19:30 +0100

    message d'erreur quand aucun resultat

--- js/result.js
+++ js/result.js
@@ -14,5 +14,11 @@ export function results(result){
             const recetteDOM = recetteModel.recetteDOM();
             recetteSection.appendChild(recetteDOM);
         });
+    } 
+    else {
+        const errorMessage = document.createElement("div");
+        errorMessage.setAttribute("class", "error-message");
+        errorMessage.innerHTML = "Aucune recette trouvée";
+        recetteSection.appendChild(errorMessage);
     }
 }
