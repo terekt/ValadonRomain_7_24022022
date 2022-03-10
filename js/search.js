128efc1426dc135487f21141d54a9425d96aec10
Romainv78 <romainvaladon@gmail.com>
2022-03-10 16:11:23 +0100

    ajout des recettes au lancement de la page

--- js/search.js
+++ js/search.js
@@ -4,12 +4,14 @@ import recipes from './recipes.js';
 
 export function search() {
     const searchinput = document.getElementById("searchinput")
+    var result = recipes;
+    results(result);
 
     searchinput.addEventListener("keyup", function () { // quand on tape une touche du clavier
         const input = searchinput.value; // récupère la valeur de la barre de recherche
 
         if (input.length > 2) { // si on a plus de 2 charactères
-            var result = recipes.filter(function (item) { // crée et retourne un nouveau tableau à partir de la base de donnée
+            result = recipes.filter(function (item) { // crée et retourne un nouveau tableau à partir de la base de donnée
                 const name = item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // récupère les recettes dont le nom correspond à la recherche
                 const description = item.description.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // description
 
@@ -19,12 +21,14 @@ export function search() {
 
                 return (name || description || ingredientList) // retourne les éléments correspondants aux 3 critères
             })
+            results(result);
         }
         else { // sinon revois une variable vide
-            result = "";
+            result = recipes;
+            results(result);
         }
 
-        results(result);
+        
 
     })
 }
