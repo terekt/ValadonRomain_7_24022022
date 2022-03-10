5d6779044bb3927ca37454ef7757d00df96f9d77
Romainv78 <romainvaladon@gmail.com>
2022-03-10 16:40:38 +0100

    fonction -> class

--- js/result.js
+++ js/result.js
@@ -3,21 +3,23 @@ import { recetteFactory } from './factory.js';
 import recipes from './recipes.js';
 
 //met à jour l'affichage des recette via le résultat de recherche
-export function results(result){
-    var recetteSection = document.querySelector(".resultat-container");
+export class results {
+    constructor(result) {
+        var recetteSection = document.querySelector(".resultat-container");
 
-    recetteSection.innerHTML = "";
-    if (result != "") { // si la recherche n'est pas vide
-        result.forEach((recette) => { // créé et insère un élément html pour chaque élément de la liste
-            const recetteModel = new recetteFactory(recette);
-            const recetteDOM = recetteModel.recetteDOM();
-            recetteSection.appendChild(recetteDOM);
-        });
-    }
-    else {
-        const errorMessage = document.createElement("div");
-        errorMessage.setAttribute("class", "error-message");
-        errorMessage.innerHTML = "Aucune recette trouvée";
-        recetteSection.appendChild(errorMessage);
+        recetteSection.innerHTML = "";
+        if (result != "") { // si la recherche n'est pas vide
+            result.forEach((recette) => { // créé et insère un élément html pour chaque élément de la liste
+                const recetteModel = new recetteFactory(recette);
+                const recetteDOM = recetteModel.recetteDOM();
+                recetteSection.appendChild(recetteDOM);
+            });
+        }
+        else {
+            const errorMessage = document.createElement("div");
+            errorMessage.setAttribute("class", "error-message");
+            errorMessage.innerHTML = "Aucune recette trouvée";
+            recetteSection.appendChild(errorMessage);
+        }
     }
 }
--- js/search.js
+++ js/search.js
@@ -1,34 +1,33 @@
 import { results } from './result.js';
 import recipes from './recipes.js';
 
+export class search {
+    constructor() {
+        const searchinput = document.getElementById("searchinput")
+        var result = recipes;
+        new results(result);
 
-export function search() {
-    const searchinput = document.getElementById("searchinput")
-    var result = recipes;
-    results(result);
+        searchinput.addEventListener("keyup", function () { // quand on tape une touche du clavier
+            const input = searchinput.value; // récupère la valeur de la barre de recherche
 
-    searchinput.addEventListener("keyup", function () { // quand on tape une touche du clavier
-        const input = searchinput.value; // récupère la valeur de la barre de recherche
+            if (input.length > 2) { // si on a plus de 2 charactères
+                result = recipes.filter(function (item) { // crée et retourne un nouveau tableau à partir de la base de donnée
+                    const name = item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // récupère les recettes dont le nom correspond à la recherche
+                    const description = item.description.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // description
 
-        if (input.length > 2) { // si on a plus de 2 charactères
-            result = recipes.filter(function (item) { // crée et retourne un nouveau tableau à partir de la base de donnée
-                const name = item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // récupère les recettes dont le nom correspond à la recherche
-                const description = item.description.toLocaleLowerCase().includes(input.toLocaleLowerCase()); // description
+                    for (let i = 0; i < item.ingredients.length; i++) { // ingrédients
+                        var ingredientList = item.ingredients[i].ingredient.toLocaleLowerCase().includes(input.toLocaleLowerCase());
+                    }
 
-                for (let i = 0; i < item.ingredients.length; i++) { // ingrédients
-                    var ingredientList = item.ingredients[i].ingredient.toLocaleLowerCase().includes(input.toLocaleLowerCase());
-                }
+                    return (name || description || ingredientList) // retourne les éléments correspondants aux 3 critères
+                })
+                new results(result);
+            }
+            else { // sinon revois une variable vide
+                result = recipes;
+                new results(result);
+            }
 
-                return (name || description || ingredientList) // retourne les éléments correspondants aux 3 critères
-            })
-            results(result);
-        }
-        else { // sinon revois une variable vide
-            result = recipes;
-            results(result);
-        }
-
-        
-
-    })
+        })
+    }
 }
